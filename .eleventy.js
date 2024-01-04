ed92bfe6e8e2539c958de47b09fff19749a55b17
Gurpreet Singh <gursingh7@publicisgroupe.net>
2024-01-04 15:12:37 +0100

    added images

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,13 @@
+const ImageShortCode = require("./11ty/responsiveImage");
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
   eleventyConfig.addPassthroughCopy("src/assets/images");
   eleventyConfig.addPassthroughCopy({
     "./src/site.webmanifest": "site.webmanifest",
   });
+
+  eleventyConfig.addNunjucksAsyncShortcode("responsiveImage", ImageShortCode);
 
   return {
     dir: {
--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # Resume of Gurpreet Singh
-
-This template can be used to create new resumes. All the data is being handled through the `_data` folder in `JSON` files.
